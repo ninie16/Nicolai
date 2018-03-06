d2669824a9ed9e15b2ff77bb030ff0bda35ec4ef
ninie16 <ninie16@student.sdu.dk>
2018-03-06 18:32:21 +0100

    commit1

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.alber.a1st_hello_world;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+Yolo!
